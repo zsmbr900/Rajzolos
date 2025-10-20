d9765bf52d8c1365f6fdc25618f9872db1e22dd9
Rendszergazda <rendszergazda@blathy.info>
2025-10-20 15:07:06 +0200

    asd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,12 +5,14 @@ namespace ConsoleApp1
     internal class Program
     {
         public static bool colorChangeMode = false;
+        public static bool helpMode = false;
+
+        public static int width = Console.WindowWidth;
+        public static int height = Console.WindowHeight;
         static void Keret()
         {
             colorChangeMode = false;
             Console.ForegroundColor = ConsoleColor.White;
-            int width = Console.WindowWidth;
-            int height = Console.WindowHeight;
 
             for (int i = 0; i < width; i++)
             {
@@ -56,15 +58,58 @@ namespace ConsoleApp1
         static void szinvalto()
         {
             Console.ForegroundColor = (ConsoleColor)(((int)Console.ForegroundColor + 1) % 16);
-            if(Console.ForegroundColor == 0) { Console.ForegroundColor = Console.ForegroundColor + 1; }
+            if (Console.ForegroundColor == 0) { Console.ForegroundColor = Console.ForegroundColor + 1; }
         }
 
-        static void Main(string[] args)
+        static void Helpablak()
         {
-            Keret();
-            do
+            Console.ForegroundColor = ConsoleColor.White;
+
+            for (int i = 0; i < width / 2; i++)
             {
-                var key = Console.ReadKey().Key;
+                Console.SetCursorPosition(i, 0);
+                Console.Write('═');
+                Console.SetCursorPosition(i, height / 2);
+                Console.Write('═');
+            }
+
+            for (int i = 0; i < height / 2 + 1 ; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                if (i == 0)
+                {
+                    Console.Write("╔");
+                }
+                else if (i == height / 2)
+                {
+                    Console.Write("╠");
+                }
+                else
+                {
+                    Console.Write('║');
+                }
+
+                Console.SetCursorPosition(width / 2, i);
+                if (i == 0)
+                {
+                    Console.Write("╦");
+                }
+                else if (i == height / 2)
+                {
+                    Console.Write("╝");
+                }
+                else
+                {
+                    Console.Write('║');
+                }
+            }
+        }
+            static void Main(string[] args)
+            {
+                Keret();
+                do
+                {
+                    var key = Console.ReadKey().Key;
 
                     switch (key)
                     {
@@ -89,7 +134,7 @@ namespace ConsoleApp1
                         case ConsoleKey.LeftArrow:
                             if (colorChangeMode) szinvalto();
                             if (Console.CursorLeft > 1)
-                            {    
+                            {
                                 Console.Write('▓');
                                 Console.CursorLeft = Console.CursorLeft - 2;
                             }
@@ -118,10 +163,22 @@ namespace ConsoleApp1
                         case ConsoleKey.PageDown:
                             colorChangeMode = !colorChangeMode;
                             break;
+                        case ConsoleKey.F1:
+                        helpMode = !helpMode;
+                        if (helpMode)
+                        {
+                            Helpablak();
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Keret();
+                        }
+                        break;
                     }
 
+                }
+                while (true);
             }
-            while (true);
-        }
     }
 }
